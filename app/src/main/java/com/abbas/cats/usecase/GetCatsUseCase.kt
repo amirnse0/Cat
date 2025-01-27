fa4a8b7b4f7f84a05643cc9dbe267f44601285a6
amirabbas <nesaei@mahsan.co>
2025-01-27 16:00:27 +0330

    add GetCatsUseCase.

---
+++ app/src/main/java/com/abbas/cats/usecase/GetCatsUseCase.kt
@@ -0,0 +1,28 @@
+package com.abbas.cats.usecase
+
+import com.abbas.cats.repository.CatsRepository
+import com.abbas.cats.usecase.converter.CatConverter
+import com.abbas.cats.usecase.presentationmodel.Cat
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+class GetCatsUseCase @Inject constructor(
+    private val catsRepository: CatsRepository
+) : UseCase<GetCatsUseCase.Request, GetCatsUseCase.Response>(
+    Configuration(Dispatchers.IO)
+) {
+    data class Request(
+        val page: Int,
+        val limit: Int,
+        val containsBreed: Boolean = true
+    ) : UseCase.Request
+
+    data class Response(val data: List<Cat>) : UseCase.Response
+
+    override suspend fun process(request: Request): Flow<Response> {
+        return catsRepository.getCats(request.page, request.limit, request.containsBreed)
+            .map { Response(it.map { cat -> CatConverter.convertPresentationCat(cat) }) }
+    }
+}
